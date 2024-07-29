9da6f647a47c00e0f687cf26332274a93db76979
jessielin34 <80348079+jessielin34@users.noreply.github.com>
2024-07-29 14:59:23 -0400

    fixed some issues with the project container cards

--- app.js
+++ app.js
@@ -1,31 +1,55 @@
 //scroll to top functionality
-const scrollUp = document.querySelector("#scroll-up");
+//const scrollUp = document.querySelector("#scroll-up");
 
-scrollUp.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth",
-    });
-});
+//scrollUp.addEventListener("click", () => {
+    //window.scrollTo({
+        //top: 0,
+        //left: 0,
+        //behavior: "smooth",
+    //});
+//});
 
 //nav hamburger selections
 
-const burger = document.querySelector("#burger-menu");
-const ul = document.querySelector("nav ul");
-const nav = document.querySelector("nav");
+//const burger = document.querySelector("#burger-menu");
+//const ul = document.querySelector("nav ul");
+//const nav = document.querySelector("nav");
 
-burger.addEventListener("click", () => {
-    ul.classList.toggle("show");
-});
+//burger.addEventListener("click", () => {
+    //console.log("Burger menu clicked");
+    //ul.classList.toggle("show");
+//});
 
 //close hamburger menu when a link is clicked
 
 //select nav links
-const navLink = document.querySelectorAll(".nav-link");
+//const navLink = document.querySelectorAll(".nav-link");
 
-navLink.forEach((link) =>
-    link.addEventListener("click", () => {
-        ul.classList.remove("show");
-    })
-);
+//navLink.forEach((link) =>
+    //link.addEventListener("click", () => {
+        //ul.classList.remove("show");
+    //})
+//);
+
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector("#burger-menu");
+    const ul = document.querySelector("nav ul");
+    //const nav = document.querySelector("nav");
+
+    if (burger && ul) {
+        burger.addEventListener("click", () => {
+            console.log("Burger menu clicked");
+            ul.classList.toggle("show");
+        });
+    }
+
+    // Close hamburger menu when a link is clicked
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach((link) => {
+        link.addEventListener("click", () => {
+            ul.classList.remove("show");
+            console.log("Menu closed on link click"); 
+        });
+    });
+});
